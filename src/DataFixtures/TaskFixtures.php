dbdd8dbbe409d2cc44ade8070e25ce8dbd88ff86
Frédéric <fvanmarcke72@gmail.com>
2022-04-06 17:46:51 +0200

    Creation of fixtures for Task and User entities and security roles

---
+++ src/DataFixtures/TaskFixtures.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Task;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class TaskFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $faker = \Faker\Factory::create('fr_FR');
+
+        for ($i = 1; $i <= 10; ++$i) {
+            $task = new Task();
+            $task->setTitle('Faire la tache '.$i)
+                ->setContent($faker->text(mt_rand(50, 150)))
+                ->setIsDone(false);
+
+            $manager->persist($task);
+        }
+
+        $manager->flush();
+    }
+}
