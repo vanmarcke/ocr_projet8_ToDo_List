70eaae2e74aa0c8a6e73c552239710e270318755
Frédéric <fvanmarcke72@gmail.com>
2022-04-12 17:38:22 +0200

    Test that the HTTP response was successful and that the request body contains the requested items

---
+++ tests/Controller/SecurityControllerTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+class SecurityControllerTest extends WebTestCase
+{
+    /**
+     * Test that the HTTP response was successful and that the request body contains the requested items
+     */
+    public function testRequestBodyContains(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/login');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+        $this->assertSelectorExists('form');
+        $this->assertSelectorTextContains('a', 'To Do List app');
+        $this->assertSame(1, $crawler->filter('html:contains("Nom d\'utilisateur :")')->count());
+        $this->assertSame(1, $crawler->filter('html:contains("Mot de passe :")')->count());
+        $this->assertCount(3, $crawler->filter('input'));
+        $this->assertSelectorTextContains('button', 'Se connecter');
+    }
+}
