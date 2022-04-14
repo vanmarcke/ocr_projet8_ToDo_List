cb483a9adb333976f5e6b774285ecd77a631d57e
Frédéric <fvanmarcke72@gmail.com>
2022-04-14 18:12:42 +0200

    Modification according to comments

--- src/Repository/TaskRepository.php
+++ src/Repository/TaskRepository.php
@@ -4,8 +4,6 @@ namespace App\Repository;
 
 use App\Entity\Task;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\ORM\OptimisticLockException;
-use Doctrine\ORM\ORMException;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -20,30 +18,6 @@ class TaskRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Task::class);
     }
-
-    // /**
-    //  * @throws ORMException
-    //  * @throws OptimisticLockException
-    //  */
-    // public function add(Task $entity, bool $flush = true): void
-    // {
-    //     $this->_em->persist($entity);
-    //     if ($flush) {
-    //         $this->_em->flush();
-    //     }
-    // }
-
-    // /**
-    //  * @throws ORMException
-    //  * @throws OptimisticLockException
-    //  */
-    // public function remove(Task $entity, bool $flush = true): void
-    // {
-    //     $this->_em->remove($entity);
-    //     if ($flush) {
-    //         $this->_em->flush();
-    //     }
-    // }
 
     /**
      * Method save.
